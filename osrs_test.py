ccc1cc602e88392774cd13cf1b451e86979eb39f
Thomas Alabaster <67207099+Tagual@users.noreply.github.com>
2020-07-25 10:43:35 +0100

    Update osrs_test.py
    Added an API search to the get_item_price instead of searching a local file.
    
    Added get_the_cost_of_item(): to tidy all the function calls.

--- osrs_test.py
+++ osrs_test.py
@@ -1,44 +1,57 @@
 
 import requests
 import json
-
+import urllib
 
 #Open the database 
 def open_database():
     """Open the database. """
-    filename = 'data/objects_osrs.json'
-    with open(filename) as f:
-        osrs_database = json.load(f)
+    with urllib.request.urlopen("https://raw.githubusercontent.com/osrsbox"
+    "/osrsbox-db/master/docs/items-summary.json") as url:
+        osrs_database = json.loads(url.read().decode())
         return osrs_database
-
 
 def request_item_name(osrs_database):
     """Obtain the item ID. """
     item_requested = input('Enter your item here: ')
-    for item_osrs in osrs_database:
-        if item_osrs['name'] == item_requested:
-            return(item_osrs['id'])
+    for x in osrs_database.values():
+        new_values = list(x.values())
+
+        if item_requested.capitalize() in new_values:
+            item_id = (new_values[0])
+            return item_id
 
 def get_item_price(id):
     """Use the OSRS API to get the item price on the GE. """
-    base_url = 'http://services.runescape.com/m=itemdb_oldschool'
-    end_url = f"/api/catalogue/detail.json?item={id}"
-    url = base_url + end_url
-
+    url = (
+        f"http://services.runescape.com/m=itemdb_oldschool/"
+        f"api/catalogue/detail.json?item={id}"
+    )
     r = requests.get(url)
-    # print(f"Status Code: {r.status_code}")
     response_url = r.json()
-
     response_url = response_url['item']
     price = response_url['current']['price']
     item_name = response_url['name']
     return(f"A {item_name} costs {price} gp.")
-    
-try:
-    new_database = open_database()
-    item_id = request_item_name(new_database)
-    final_response = get_item_price(item_id)
-    print(final_response)
-except ValueError:
-    print("You spelt the item incorrectly, or the item is not tradeable.")
 
+def get_the_cost_of_item():
+    try:
+        new_database = open_database()
+        item_id = request_item_name(new_database)
+        final_response = get_item_price(item_id)
+        print(final_response)
+    except ValueError:
+        print("You spelt the item incorrectly, or the item "
+        "is not tradeable.")
+
+
+get_the_cost_of_item()
+    
+while True:
+    user_input = input("Do you still want to find the price of "
+    "items? (y/n) ")
+    if user_input.lower() == 'y':
+        get_the_cost_of_item()
+    else: 
+        break
+print("Thanks, see you soon")
