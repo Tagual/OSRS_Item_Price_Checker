ed94bfe430388c2c0824078135e3196339b07c69
Thomas Alabaster <67207099+Tagual@users.noreply.github.com>
2020-07-27 15:03:23 +0100

    Update osrs_test.py
    Allowed multiple items to be searched for at the same time

--- osrs_test.py
+++ osrs_test.py
@@ -14,35 +14,44 @@ def open_database():
 def request_item_name(osrs_database):
     """Obtain the item ID. """
     item_requested = input('Enter your item here: ')
-    for x in osrs_database.values():
-        new_values = list(x.values())
+    item_ids = []
+    for item in osrs_database.values():
+        if item_requested.lower() in item['name'].lower():
+            item_ids.append(item['id'])
+    return item_ids
+    
 
-        if item_requested.capitalize() in new_values:
-            item_id = (new_values[0])
-            return item_id
-
-def get_item_price(id):
-    """Use the OSRS API to get the item price on the GE. """
-    url = (
-        f"http://services.runescape.com/m=itemdb_oldschool/"
-        f"api/catalogue/detail.json?item={id}"
-    )
-    r = requests.get(url)
-    response_url = r.json()
-    response_url = response_url['item']
-    price = response_url['current']['price']
-    item_name = response_url['name']
-    return(f"A {item_name} costs {price} gp.")
+def get_item_price(item_ID):
+    for object_ID in item_ID:
+        """Use the OSRS API to obtain item price on the GE. """
+        try:
+            url = (
+                f"http://services.runescape.com/m=itemdb_oldschool/"
+                f"api/catalogue/detail.json?item={object_ID}"
+            )
+            r = requests.get(url)
+            response_url = r.json()
+            response_url = response_url['item']
+            price = response_url['current']['price']
+            item_name = response_url['name']
+            (print(f"{item_name} costs {price} gp."))
+        except: 
+            pass
+    return
 
 def get_the_cost_of_item():
     try:
         new_database = open_database()
         item_id = request_item_name(new_database)
-        final_response = get_item_price(item_id)
-        print(final_response)
+        if item_id == []:
+            return print("You have spelt the item incorrectly or it is not "
+            "tradeable")
+        get_item_price(item_id)
     except ValueError:
+        pass
         print("You spelt the item incorrectly, or the item "
         "is not tradeable.")
+    return
 
 
 get_the_cost_of_item()
@@ -55,3 +64,6 @@ while True:
     else: 
         break
 print("Thanks, see you soon")
+
+
+
